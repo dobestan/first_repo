db086c3ea8432f63efff0a3e2e86811eea590fb5
Suchan An <me@ansuchan.com>
2017-03-24 14:47:52 +0900

    init web app

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    port = os.environ.get("PORT", 5000)
+    app.run("0.0.0.0", port)
